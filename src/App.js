1b12dc097b667fbdd83e084c7075c13ff9bd80d2
LSH7875 <tmdgml7875@gmail.com>
2022-05-18 08:15:47 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a
+          className="App-link"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Purple State Dao
+        </a>
+        <div className="linear-line"></div>
+        <p>comming soon</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
